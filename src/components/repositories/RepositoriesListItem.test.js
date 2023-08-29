8e0ddea5a431396519b7b918ced37ae4c11fc65f
jules2m1998 <mevaajules9@gmail.com>
2023-08-29 11:43:16 +0400

    :wrench:

--- src/components/repositories/RepositoriesListItem.test.js
+++ src/components/repositories/RepositoriesListItem.test.js
@@ -1,4 +1,4 @@
-import { render, screen, act } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import RepositoriesListItem from "./RepositoriesListItem";
 import { MemoryRouter } from "react-router";
 import { async } from "validate.js";
@@ -8,7 +8,9 @@ function renderComponent() {
     full_name: "facebook/react",
     language: "Javascript",
     description: "A js library",
-    owner: "facebook",
+    owner: {
+      login: "facebook",
+    },
     name: "react",
     html_url: "https://github.com/facebook/react",
   };
@@ -17,18 +19,37 @@ function renderComponent() {
       <RepositoriesListItem repository={repository} />
     </MemoryRouter>
   );
+
+  return { repository };
 }
-const pause = () => {
-  return new Promise((resolve) => {
-    setTimeout(resolve, 100);
-  });
-};
 
 describe("RepositoriesListItem", () => {
   test("show a link to the github homepage for this repository", async () => {
-    renderComponent();
-    await act(async () => {
-      await pause();
+    const { repository } = renderComponent();
+    await screen.findAllByRole("img", { name: repository.language });
+
+    const link = screen.getByRole("link", {
+      name: /github repository/i,
     });
+    expect(link).toHaveAttribute("href", repository.html_url);
+  });
+
+  test("shows a fileicon with the appropriate icon", async () => {
+    const { repository } = renderComponent();
+    const icon = await screen.findByRole("img", { name: repository.language });
+    expect(icon).toHaveClass("js-icon");
+  });
+
+  test("shows the link to the code editor page", async () => {
+    const { repository } = renderComponent();
+    await screen.findByRole("img", { name: repository.language });
+    const link = await screen.findByRole("link", {
+      name: new RegExp(repository.owner.login),
+    });
+
+    expect(link).toHaveAttribute(
+      "href",
+      `/repositories/${repository.full_name}`
+    );
   });
 });
